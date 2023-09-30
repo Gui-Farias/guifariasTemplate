a53f44e406e419fda1b30de02c96cde6b0434cfa
Guilherme Farias <37490430+Gui-Farias@users.noreply.github.com>
2023-09-30 16:41:48 -0300

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,3 @@
-<!-- <script>
-	import About from '$lib/template/About/About.svelte';
-	import Hero from '$lib/template/Hero/Hero.svelte';
-	import ServiceSkils from '$lib/template/ServiceSkils/ServiceSkils.svelte';
-	import Work from '$lib/template/work/Work.svelte';
-</script>
-
-<Hero />
-<About />
-<ServiceSkils />
-<Work /> -->
-
 <h1>
 	Hi mom!
 </h1>
